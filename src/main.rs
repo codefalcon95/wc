0cd4aafc0b0abd70476c95047603b34082a52e14
Sanni Moshood Abiola <sanniabiolamoshood95@gmail.com>
2024-02-11 10:16:10 +0100

    feat(wc): added other functions

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 mod wc;
 use std::env;
+use std::fs;
+use std::io;
+use std::io::prelude::*;
+use std::time::{SystemTime, UNIX_EPOCH};
 use wc::wc::process_file;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Please provide a filename.");
-        return;
+    if args.len() < 2 { println!("Please provide a filename."); return; }
+
+    let default_args: String = String::new();
+    let command: &str = args.get(1).unwrap_or(&default_args).as_str();
+    let mut file_path: &str = args.get(2).unwrap_or(&default_args).as_str();
+
+    if file_path == default_args { file_path = args.get(1).unwrap().as_str(); }
+
+    let mut temp_filename = String::new();
+    let mut show_file_path = true;
+
+    if !fs::metadata(file_path).is_ok() {
+        let mut buffer = String::new();
+        io::stdin().read_to_string(&mut buffer).unwrap();
+
+        if !buffer.is_empty() {
+            let now = match SystemTime::now().duration_since(UNIX_EPOCH) {
+                Ok(n) => n.as_secs(),
+                Err(_) => 0,
+            };
+            temp_filename = format!("{}_{}.txt", file_path, now);
+            let mut file = fs::File::create(&temp_filename).expect("Unable to create file");
+            file.write_all(buffer.as_bytes())
+                .expect("Unable to write data");
+            file_path = temp_filename.as_str();
+            show_file_path = false;
+        }
     }
-    let filename: &str = &args[1];
-    let commands: Vec<&str> = args.iter().skip(2).map(String::as_str).collect();
 
-   process_file(filename, commands);
-
+    process_file(command, file_path, show_file_path);
+    
+    if fs::metadata(&temp_filename).is_ok() {
+        fs::remove_file(temp_filename).unwrap();
+    }
 }
--- src/wc.rs
+++ src/wc.rs
@@ -1,24 +1,87 @@
-
 pub mod wc {
     use std::fs;
+    use std::io::prelude::*;
+    use std::io::BufReader;
 
-  pub fn process_file(file_path: &str, commands: Vec<&str>) {
-    let file_size = get_file_size_in_byte(file_path);
-    println!("{} {}", file_size, file_path);
-  }
+    #[derive(PartialEq)]
+    enum Command {
+        Line,
+        Word,
+        Size,
+        Character,
+        All,
+    }
 
-  fn get_file_size_in_byte(file_path: &str) -> u64 {
-    let f = match fs::metadata(file_path) {
-      Ok(metadata) => {
-        metadata
-      },
-      Err(e) => {
-        println!("Error: {}", e);
-        return 0;
-      }
+    pub fn process_file(command: &str, file_path: &str, show_file_path: bool) {
+        let command = process_wc_command(command);
+        let file_path_to_print = if show_file_path { file_path } else { "" };
+        match command {
+            Command::Size => {
+                let file_size = get_file_size_in_byte(file_path);
+                println!("{} {}", file_size, file_path_to_print);
+            }
+            Command::Line => {
+                let lines = get_lines_in_file(file_path);
+                println!("{:?} {}", lines, file_path_to_print);
+            }
+            Command::Word => {
+                let words = get_words_in_file(file_path);
+                println!("{:?} {}", words, file_path_to_print);
+            }
+            Command::Character => {
+                let characters = get_characters_in_file(file_path);
+                println!("{:?} {}", characters, file_path_to_print);
+            }
+            _ => {
+                let file_size = get_file_size_in_byte(file_path);
+                let lines = get_lines_in_file(file_path);
+                let words = get_words_in_file(file_path);
+                println!(" {} {} {} {}", lines, words, file_size, file_path_to_print);
+            }
+        }
+    }
 
-   }; 
+    fn get_file_size_in_byte(file_path: &str) -> u64 {
+        let f = match fs::metadata(file_path) {
+            Ok(metadata) => metadata,
+            Err(e) => {
+                println!("Error: {}", e);
+                return 0;
+            }
+        };
+        f.len()
+    }
 
-    return f.len();
-  }
+    fn process_wc_command(command: &str) -> Command {
+        match command {
+            "-l" => Command::Line,
+            "-w" => Command::Word,
+            "-c" => Command::Size,
+            "-m" => Command::Character,
+            _ => Command::All,
+        }
+    }
+
+    fn get_lines_in_file(file_path: &str) -> usize {
+        let f = fs::File::open(file_path).unwrap();
+        let reader = BufReader::new(f);
+        let total_lines = reader.lines().count();
+        total_lines
+    }
+
+    fn get_words_in_file(file_path: &str) -> usize {
+        let f = fs::File::open(file_path).unwrap();
+        let mut reader = BufReader::new(f);
+        let mut buff = String::new();
+        let _ = reader.read_to_string(&mut buff);
+        buff.split_whitespace().count()
+    }
+
+    fn get_characters_in_file(file_path: &str) -> usize {
+        let f = fs::File::open(file_path).unwrap();
+        let mut reader = BufReader::new(f);
+        let mut buff = String::new();
+        let _ = reader.read_to_string(&mut buff);
+        buff.chars().count()
+    }
 }
